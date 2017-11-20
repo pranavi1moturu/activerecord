9eaedbdf4b4a6d9f6ba5588946ae11f8fbb6d515
pranavi moturu <pm487@afsconnect1.njit.edu>
2017-11-19 20:36:57 -0500

    updated index1.php

--- index1.php
+++ index1.php
@@ -1,7 +1,4 @@
 <?php
-//turn on debugging messages
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
 define('DATABASE', 'pm487');
 define('USERNAME', 'pm487');
 define('PASSWORD', 'kKmilXOt');
@@ -35,37 +32,37 @@ class dbConn{
     }
 }
 class collection
- {
+{
 protected $html;
-    static public function create() 
-    {
-      $model = new static::$modelName;
-      return $model;
-    }
-    static public function findAll()
-     {
-        $db = dbConn::getConnection();
-        $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName;
-        $statement = $db->prepare($sql);
-        $statement->execute();
-        $class = static::$modelName;
-        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
-        $recordsSet =  $statement->fetchAll();
-        return $recordsSet;
-    }
-    static public function findOne($id)
-     {
-        $db = dbConn::getConnection();
-        $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
-        $statement = $db->prepare($sql);
-        $statement->execute();
-        $class = static::$modelName;
-        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
-        $recordsSet =  $statement->fetchAll();
-        return $recordsSet;
-    }
+static public function create() 
+{
+$model = new static::$modelName;
+return $model;
+}
+static public function findAll()
+{
+$db = dbConn::getConnection();
+$tableName = get_called_class();
+$sql = 'SELECT * FROM ' . $tableName;
+$statement = $db->prepare($sql);
+$statement->execute();
+$class = static::$modelName;
+$statement->setFetchMode(PDO::FETCH_CLASS, $class);
+$recordsSet =  $statement->fetchAll();
+return $recordsSet;
+}
+static public function findOne($id)
+{
+$db = dbConn::getConnection();
+$tableName = get_called_class();
+$sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
+$statement = $db->prepare($sql);
+$statement->execute();
+$class = static::$modelName;
+$statement->setFetchMode(PDO::FETCH_CLASS, $class);
+$recordsSet =  $statement->fetchAll();
+return $recordsSet;
+}
 }
 class accounts extends collection
  {
@@ -81,149 +78,154 @@ class model
 protected $tableName;
 public function save()
     
-    {
-        if ($this->id != '')
-         {
-            $sql = $this->update($this->id);
-        } 
-        else 
-        {
-           $sql = $this->insert();
-        }
-        $db = dbConn::getConnection();
-        $statement = $db->prepare($sql);
-        $array = get_object_vars($this);
-        foreach (array_flip($array) as $key=>$value)
-        {
-            $statement->bindParam(":$value", $this->$value);
-        }
-        $statement->execute();
+{
+if ($this->id != '')
+{
+ $sql = $this->update($this->id);
+ } 
+ else 
+ {
+ $sql = $this->insert();
+ }
+ $db = dbConn::getConnection();
+ $statement = $db->prepare($sql);
+ $array = get_object_vars($this);
+ foreach (array_flip($array) as $key=>$value)
+ {
+ $statement->bindParam(":$value", $this->$value);
+ }
+ $statement->execute();
+ }
+ private function insert()
+ {
+ $modelName=get_called_class();
+ $tableName = $modelName::getTablename();
+ $array = get_object_vars($this);
+ $data = implode(',', array_flip($array));
+ $values = ':'.implode(',:', array_flip($array));
+  print_r($data);
+  $sql =  'INSERT INTO '.$tableName.' ('.$data.') VALUES ('.$values.')';
+  return $sql;
     }
-    private function insert()
-     {
-        $modelName=get_called_class();
-        $tableName = $modelName::getTablename();
-        $array = get_object_vars($this);
-        $columnString = implode(',', array_flip($array));
-        $valueString = ':'.implode(',:', array_flip($array));
-        print_r($columnString);
-        $sql =  'INSERT INTO '.$tableName.' ('.$columnString.') VALUES ('.$valueString.')';
-        return $sql;
-    }
-    private function update($id) 
-    {
-        $modelName=get_called_class();
-        $tableName = $modelName::getTablename();
-        $array = get_object_vars($this);
-        $comma = " ";
-        $sql = 'UPDATE '.$tableName.' SET ';
-        foreach ($array as $key=>$value){
-            if( ! empty($value)) {
-                $sql .= $comma . $key . ' = "'. $value .'"';
-                $comma = ", ";
-            }
-        }
-        $sql .= ' WHERE id='.$id;
-        return $sql;
-    }
-    public function delete($id) 
-    {
-        $db = dbConn::getConnection();
-        $modelName=get_called_class();
-        $tableName = $modelName::getTablename();
-        $sql = 'DELETE FROM '.$tableName.' WHERE id='.$id;
-        $statement = $db->prepare($sql);
-        $statement->execute();
+  private function update($id) 
+   {
+   $modelName=get_called_class();
+   $tableName = $modelName::getTablename();
+   $array = get_object_vars($this);
+   $comma = " ";
+   $sql = 'UPDATE '.$tableName.' SET ';
+   foreach ($array as $key=>$value)
+   {
+     if( ! empty($value)) 
+   {
+   $sql .= $comma . $key . ' = "'. $value .'"';
+  $comma = ", ";
+  }
+}
+$sql .= ' WHERE id='.$id;
+return $sql;
+}
+ public function delete($id) 
+ {
+ $db = dbConn::getConnection();
+ $modelName=get_called_class();
+ $tableName = $modelName::getTablename();
+$sql = 'DELETE FROM '.$tableName.' WHERE id='.$id;
+$statement = $db->prepare($sql);
+$statement->execute();
     }
 }
     
 
 class account extends model 
 {
-    public $id;
-    public $email;
-    public $fname;
-    public $lname;
-    public $phone;
-    public $birthday;
-    public $gender;
-    public $password;
-    public static function getTablename()
-    {
-        $tableName='accounts';
-        return $tableName;
+public $id;
+public $email;
+public $fname;
+public $lname;
+public $phone;
+public $birthday;
+public $gender;
+public $password;
+public static function getTablename()
+{
+$tableName='accounts';
+return $tableName;
     }
 }
 
 class todo extends model
  {
-    public $id;
-    public $owneremail;
-    public $ownerid;
-    public $createddate;
-    public $duedate;
-    public $message;
-    public $isdone;
-    public static function getTablename(){
+ public $id;
+ public $owneremail;
+ public $ownerid;
+ public $createddate;
+public $duedate;
+ public $message;
+public $isdone;
+public static function getTablename()
+{
         $tableName='todos';
         return $tableName;
     }
 }
 //Todos table
+echo "<div align = 'right'> </div>";
  echo  "<font size = 4> Search for all records in  todo table </font>";
  $records = todos::findAll();  
-  $html = '<table border =5>';
+  $d = '<table border =5>';
   
-  $html .= '<tr>';
+  $d .= '<tr>';
     foreach($records[0] as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
+    $d .= '</tr>';
     
     foreach($records as $key=>$value)
     {
-        $html .= '<tr>';
+        $d .= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
       
   
     }
-    $html .= '</table>';
+    $d .= '</table>';
 
-    print_r($html);
+    print_r($d);
+   
 //finding one record in todos table
     echo "<font size= 4 >Search for one record by id</font>";
  $record = todos::findOne(4);
   
-  print_r( "Todo table id - 4");
-  $html = '<table border =5>';
-  $html .= '<tr>';
+  
+  $d = '<table border =5>';
+  $d .= '<tr>';
     foreach($record[0]as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
-    $html .= '</tr>';
+    $d.= '</tr>';
     foreach($record as $key=>$value)
     {
-       $html .= '<tr>';
+       $d .= '<tr>';
         
-       foreach($value as $key2=>$value2)
+       foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d.= '</tr>';
     }
-    $html .= '</table>';
+    $d .= '</table>';
     
-    print_r($html);
+    print_r($d);
     
-    //delete one record in todo table
+   //delete one record in todo table
 echo  "<font size=5 >deleting one record</font>";
 $record= new todo();
 $id=42;
@@ -231,29 +233,29 @@ $record->delete($id);
 echo '<h2>Record with id: '.$id.' is deleted</h2>';
 //'<h3>Todos table After the record is Deleted</h3>';
 $record = todos::findAll();
-$html = '<table border = 5>';
+$d = '<table border = 5>';
   
-  $html .= '<tr>';
+  $d .= '<tr>';
     
     foreach($record[0] as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
+    $d .= '</tr>';
     foreach($record as $key=>$value)
     {
-        $html .= '<tr>';
+        $d .= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
     }
-    $html .= '</table>';
+    $d .= '</table>';
 echo "<h2>Todos table after the record with specified id is deleted</h2>";
-print_r($html);
+print_r($d);
 
 //update one record in todos table
 echo "<font size=5 >Update one record in todos table</font>";
@@ -270,118 +272,116 @@ $record->save();
 $record = todos::findAll();
 echo "<h2>Record update with id: ".$id."</h2>";
         
-$html = '<table border = 5>';
+$d = '<table border = 5>';
   
-  $html .= '<tr>';
+  $d.= '<tr>';
     
     foreach($record[0] as $key=>$value)
         {
             $html .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
+    $d .= '</tr>';
     foreach($record as $key=>$value)
     {
-        $html .= '<tr>';
+        $d .= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
     }
-    $html .= '</table>';
+    $d .= '</table>';
  echo  "<font size=4> Todos table with specified id is updated </font>";
- print_r($html);
+ print_r($d);
 
 //insert record
    echo "<font size=4> Insert One Record in Todos table</font>";
-        $record = new todo();
-        $record->owneremail="ght@njit.edu";
-        $record->ownerid=4;
-        $record->createddate="09-10-1994";
-        $record->duedate="10-13-1994";
-        $record->message="inserted record";
-        $record->isdone=1;
-        $record->save();
-        $records = todos::findAll();
-     $html = '<table border = 5>';
-      $html .= '<tr>';
-      foreach($records[0] as $key=>$value)
-         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
-        }
+$record = new todo();
+$record->owneremail="ght@njit.edu";
+$record->ownerid=4;
+$record->createddate="09-10-1994";
+$record->duedate="10-13-1994";
+$record->message="inserted record";
+$record->isdone=1;
+$record->save();
+$records = todos::findAll();
+$d = '<table border = 5>';
+$d .= '<tr>';
+foreach($records[0] as $key=>$value)
+{
+$d.= '<th>' . htmlspecialchars($key) . '</th>';
+}
        
-    $html .= '</tr>';
-    foreach($records as $key=>$value)
-    {
-        $html .= '<tr>';
+$d .= '</tr>';
+foreach($records as $key=>$value)
+{
+$d .= '<tr>';
         
-        foreach($value as $key2=>$value2)
-        {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
-        }
-        $html .= '</tr>';
-    }
-    echo "<h3> After Inserting one record</h3>";
-    $html .= '</table>';
-print_r($html);
+foreach($value as $key1=>$value1)
+{
+$d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
+}
+$d.= '</tr>';
+}
+echo "<h3> After Inserting one record</h3>";
+$d .= '</table>';
+print_r($d);
 
 echo "<h1>Search for all records in accounts table</h1>";
 $records = accounts::findAll();
   
-  $html = '<table border = 5>';
-
+  $d = '<table border = 5>';
   
-  $html .= '<tr>';
+  $d .= '<tr>';
     foreach($records[0] as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
-
+    $d.= '</tr>';
     foreach($records as $key=>$value)
     {
-        $html .= '<tr>';
+        $d .= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
     }
-    $html .= '</table>';
-    print_r($html);
+    $d .= '</table>';
+    print_r($d);
     
     //finding one record in accounts table
     echo"<h1>Search for one record in accounts table by id</h1>";
 $record = accounts::findOne(2);
   
-  $html = '<table border = 6>';
-  $html .= '<tr>';
+  $d = '<table border = 6>';
+  $d .= '<tr>';
     
     foreach($record[0]as $key=>$value)
         {
             $html .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
+    $d .= '</tr>';
 
     
     foreach($record as $key=>$value)
     {
-       $html .= '<tr>';
+       $d .= '<tr>';
         
-       foreach($value as $key2=>$value2)
+       foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d.= '</tr>';
     }
-    $html .= '</table>';
+    $d .= '</table>';
     
-    print_r($html);
+    print_r($d);
 
 //deleting one record in accounts table
 echo "<h1>Deleting  One Record in accounts table</h1>";
@@ -391,30 +391,30 @@ $record->delete($id);
 echo '<h3>Record with id: '.$id.' is deleted</h3>';
 $record = accounts::findAll();
 
-$html = '<table border = 5>';
+$d= '<table border = 5>';
  
   
-  $html .= '<tr>';
+  $d .= '<tr>';
     
     foreach($record[0] as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
+    $d .= '</tr>';
     foreach($record as $key=>$value)
     {
-        $html .= '<tr>';
+        $d.= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d.= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
     }
-    $html .= '</table>';
-echo "<h2>Accounts table after deleteing one record</h2>";
-print_r($html);
+    $d .= '</table>';
+echo "<h2>Accounts table after deleting one record</h2>";
+print_r($d);
 
 //updating record in accounts table
 echo "<h2>Updating one record in accounts table</h1>";
@@ -428,29 +428,29 @@ $record->save();
 $record = accounts::findAll();
 echo "<h2>Record is updated with id: ".$id."</h3>";
         
-$html = '<table border = 5>';
+$d = '<table border = 5>';
 
-  $html .= '<tr>';
+  $d .= '<tr>';
     
     foreach($record[0] as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
        
-    $html .= '</tr>';
+    $d .= '</tr>';
     foreach($record as $key=>$value)
     {
-        $html .= '<tr>';
+        $d.= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
     }
-    $html .= '</table>';
+    $d .= '</table>';
  
- print_r($html);
+ print_r($d);
  
  //Inserting one record in accounts table
  echo "<h2> Inserting one record in accounts table</h2>";
@@ -464,26 +464,25 @@ $record->gender="male";
 $record->password="158";
 $record->save();
 $records = accounts::findAll();
-$html = '<table border = 5>';
-  $html .= '<tr>';
+$d = '<table border = 5>';
+  $d .= '<tr>';
     foreach($records[0] as $key=>$value)
         {
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            $d .= '<th>' . htmlspecialchars($key) . '</th>';
         }
-    $html .= '</tr>';
+    $d .= '</tr>';
     foreach($records as $key=>$value)
     {
-        $html .= '<tr>';
+        $d .= '<tr>';
         
-        foreach($value as $key2=>$value2)
+        foreach($value as $key1=>$value1)
         {
-            $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
+            $d .= '<td>' . htmlspecialchars($value1) . '<br></td>';
         }
-        $html .= '</tr>';
+        $d .= '</tr>';
     }
-    $html .= '</table>';
+    $d .= '</table>';
 echo "<h2>After Inserting one record in accounts table </h2>";
-print_r($html);
-
+print_r($d);
 
 ?>
